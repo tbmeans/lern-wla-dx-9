16c6ea9df68f98774b7bd406b7f84add17e3b34b
Timothy Means <tbmeans2005@gmail.com>
2015-12-31 13:00:00 -0500

    noting syntax quirks

---
+++ test.s
@@ -0,0 +1,6 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;  WLA-DX learning & testing journal  ;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+... USING VARIABLES FOR INDIRECTION OPS MAKES WLA CRASH
+; ONLY WHEN I CHANGED VARIABLES TO ACTUAL NUMERICAL VALUES DID WLA COMPILE.
+; IT ALSO LIKES TO CRASH FOR LOWERCASE MNEMONICS
